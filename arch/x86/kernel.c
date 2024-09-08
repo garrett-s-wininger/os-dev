f91f95d6804ab3d29485e3ab704e65afcc08b546
Garrett Wininger <garrett.s.wininger@outlook.com>
2024-09-08 13:29:27 -0400

    Clearing of VGA 80x25 text mode screen

--- arch/x86/kernel.c
+++ arch/x86/kernel.c
@@ -1,4 +1,21 @@
+#include <stdint.h>
+
+#define VGA_WIDTH 80
+#define VGA_HEIGHT 25
+
+volatile uint16_t* vga = (uint16_t*)0xB8000;
+
+void vga_clear_screen(void) {
+    for (unsigned int x = 0; x < VGA_WIDTH; ++x) {
+        for (unsigned int y = 0; y < VGA_HEIGHT; ++y) {
+            vga[(VGA_WIDTH * y) + x] = 0;
+        }
+    }
+}
+
 void kmain(void) {
+    vga_clear_screen();
+
     for (;;) {
         // Hang on purpose
     }
